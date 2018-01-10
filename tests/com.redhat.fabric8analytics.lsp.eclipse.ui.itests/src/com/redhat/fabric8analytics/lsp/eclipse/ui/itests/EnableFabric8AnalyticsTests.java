0ecfcc072004996dab8ae027d31275dff812a983
vprusa <vprusa@redhat.com>
2018-01-10 14:54:36 +0100

    Added OSIO login using jboss requirement and test for it

---
+++ tests/com.redhat.fabric8analytics.lsp.eclipse.ui.itests/src/com/redhat/fabric8analytics/lsp/eclipse/ui/itests/EnableFabric8AnalyticsTests.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat Inc..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Incorporated - initial API and implementation
+ *******************************************************************************/
+package com.redhat.fabric8analytics.lsp.eclipse.ui.itests;
+
+import org.eclipse.reddeer.common.logging.Logger;
+import org.eclipse.reddeer.junit.runner.RedDeerSuite;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.redhat.fabric8analytics.lsp.eclipse.ui.itests.requirements.OSIOLoginRequirement.OSIOLogin;
+
+@RunWith(RedDeerSuite.class)
+@OSIOLogin
+public class EnableFabric8AnalyticsTests {
+
+	private static final Logger log = Logger.getLogger(EnableFabric8AnalyticsTests.class);
+
+	@BeforeClass
+	public static void prepare() {
+
+	}
+
+	@AfterClass
+	public static void clean() {
+		// new ProjectExplorer().deleteAllProjects(false);
+		// WorkbenchShellHandler.getInstance().closeAllNonWorbenchShells();
+	}
+
+	@Test
+	public void enableFabric8AnalyticsButtonTest() {
+		log.info("Just check if requirement is satisfied");
+	}
+
+}
---
+++ tests/com.redhat.fabric8analytics.lsp.eclipse.ui.itests/src/com/redhat/fabric8analytics/lsp/eclipse/ui/itests/dialogs/OSIOLoginDialog.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat Inc..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Incorporated - initial API and implementation
+ *******************************************************************************/
+package com.redhat.fabric8analytics.lsp.eclipse.ui.itests.dialogs;
+
+import org.eclipse.reddeer.common.logging.Logger;
+import org.eclipse.reddeer.common.wait.TimePeriod;
+import org.eclipse.reddeer.common.wait.WaitUntil;
+import org.eclipse.reddeer.common.wait.WaitWhile;
+import org.eclipse.reddeer.swt.impl.browser.InternalBrowser;
+import org.eclipse.reddeer.swt.impl.button.OkButton;
+import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
+import org.eclipse.reddeer.swt.impl.toolbar.DefaultToolItem;
+import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
+import org.eclipse.reddeer.workbench.impl.shell.WorkbenchShell;
+import org.jboss.tools.openshift.reddeer.condition.BrowserContainsText;
+
+public class OSIOLoginDialog {
+	// https://github.com/jbosstools/jbosstools-openshift/blob/master/itests/org.jboss.tools.openshift.ui.bot.test/src/org/jboss/tools/openshift/ui/bot/test/integration/openshift/io/GetOpenShiftIOTokenTest.java
+	
+	protected static final String CONTEXT_MENU_ITEM_TEXT = "Exit"; // TODO change
+
+	protected DefaultShell browser;
+
+	private static final Logger log = Logger.getLogger(OSIOLoginDialog.class);
+
+	OSIOLoginDialog() {
+		new DefaultToolItem(new WorkbenchShell(), CONTEXT_MENU_ITEM_TEXT).click();
+		browser = new DefaultShell();
+	}
+
+	public static OSIOLoginDialog openLoginDialog() {
+		return new OSIOLoginDialog();
+	}
+	
+	public void waitWhileLoading(String text) {
+		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
+		new WaitUntil(new BrowserContainsText(text), TimePeriod.LONG);
+	}
+
+	public void login() {
+		InternalBrowser internalBrowser = new InternalBrowser(browser);
+		waitWhileLoading("OpenShift.io Developer Preview");
+
+		// by account provider
+		switch (System.getProperty("OSLoginProvider")) {
+		case "JBossDeveloper":
+			internalBrowser.execute("document.getElementById(\"social-jbossdeveloper\").click()");
+			log.info("Waiting for JBossDeveloper portal");
+			internalBrowser = new InternalBrowser(browser);
+			waitWhileLoading("JBoss<strong>Developer</strong>");
+			
+			internalBrowser.execute(String.format("document.getElementById(\"username\").value=\"%s\"",
+					System.getProperty("OSusername")));
+			internalBrowser.execute(String.format("document.getElementById(\"password\").value=\"%s\"",
+					System.getProperty("OSpassword")));
+
+			internalBrowser.execute("document.getElementById(\"fm1\").submit.click()");
+
+			break;
+		default:
+			// internalBrowser.execute(String.format("document.getElementById(\"username\").value=\"%s\"",
+			// DatastoreOS3.OPENSHIFT_IO_USERNAME));
+			// internalBrowser.execute(String.format("document.getElementById(\"password\").value=\"%s\"",
+			// DatastoreOS3.OPENSHIFT_IO_PASSWORD));
+			// internalBrowser.execute(
+			// "document.getElementById(\"password\").parentElement.parentElement.parentElement.submit()");
+
+		}
+
+		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
+		new DefaultShell("OpenShift.io");
+		new OkButton().click();
+	}
+
+}
